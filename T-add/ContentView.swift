71fcc77212cfada54202a058d8a2d26261d1ecd2
e <e>
2026-02-12 11:28:38 +0300

    Initial Commit

---
+++ T-add/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  T-add
+//
+//  Created by Pavel Korostelev on 12.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ T-add/T_addApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_addApp.swift
+//  T-add
+//
+//  Created by Pavel Korostelev on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct T_addApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
